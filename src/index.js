c2bef19d431bcf04095ab7b1fa82f1d26b343f0a
jelka2008 <jelka2008.rambler.ru>
2020-10-14 15:10:33 +0300

    feat: implement task resolve

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix) return [];
+    let i = -1;
+    return matrix.reduce((rezArray, itemArray) => {
+        i++;
+        return rezArray.concat(i % 2 !== 1 ? itemArray : itemArray.reverse());
+    }, []);
+};
